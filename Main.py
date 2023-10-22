a553c3f0ee564be1608c6ebe49eb9e5f6c38cc35
Angel Adrian Alvarez Flores <angel.alvarezf@uanl.edu.mx>
2023-10-22 13:30:01 -0600

    Notas sobre Nmap

--- Main.py
+++ Main.py
@@ -80,3 +80,8 @@ if (__name__ == '__main__'):
 https://stackoverflow.com/questions/6718633/python-regular-expression-again-match-url
 """
 #Realizado en Github por: Angelaf2005
+"""Notas sobre Nmap:
+Muy probablemente no funcione desde tu computador
+para que lo haga tienes que realizar 2 cosas:
+1) Descargar nmap desde su página web
+2) Agregar la dirección de donde se instaló nmap a la variable de entorno Path desde cmd"""
