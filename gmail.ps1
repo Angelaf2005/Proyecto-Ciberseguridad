45209547ed5dbede2a285fa2f75c1f6e1979ad42
Angel Adrian Alvarez Flores <angel.alvarezf@uanl.edu.mx>
2023-10-27 15:14:09 -0600

    Otra prueba
    Acceso de gmail

--- gmail.ps1
+++ gmail.ps1
@@ -4,7 +4,5 @@ function credencial {
     return $credenciales
 }
 function correo($cred) {
-    Send-MailKitMessage -SMTPServer smtp.gmail.com -Port 587 -From "angel.alvflopia@gmail.com" -Recipientlist "adrian.alvflo201@gmail.com" -Subject tema -TextBody texto -Credential $cred;
+    Send-MailKitMessage -SMTPServer smtp.gmail.com -Port 587 -From $cred.UserName -Recipientlist "adrian.alvflo201@gmail.com" -Subject tema -TextBody texto -Credential $cred;
 }
-$c = credencial
-correo($c)
